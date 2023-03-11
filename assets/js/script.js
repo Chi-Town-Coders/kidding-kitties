5d5ad8c205ece1d318bc03c993d27ce9963a3e09
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-03-11 16:39:44 -0600

    fixed JS to appear 1 joke per image, fixed the footer to appear on the bottom of the page not in the top, added some borders to the images in CSS, and changed background color

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 // Replace these with your key/secret
 var key = 'i1H9NJybBiX2WXs257QX7yUzCR0ixMDP5JdxHhOK0dX6qEcFGp';
 var secret = 'qkwXTx7MPxTxfnJhmsQnGqwmnMAAztGEpzWqWnD4';
+
 // Call the API
 // This is a POST request, because we need the API to generate a new token for us
 fetch('https://api.petfinder.com/v2/oauth2/token', {
@@ -28,70 +29,82 @@ fetch('https://api.petfinder.com/v2/oauth2/token', {
 	// Return the API response as JSON
 	return resp.json();
 }).then(function (data) {
-    // Log the pet data
+	// Log the pet data
 	console.log('pets', data);
 
-    var catImages = data.animals
-    .filter(function (animal) {
-      return animal.photos.length > 0;
-    })
-    .map(function (animal) {
-      return {
-        url: animal.url,
-        photos: animal.photos[0].medium
-		
+	var catImages = data.animals
+		.filter(function (animal) {
+			return animal.photos.length > 0;
+		})
+		.map(function (animal) {
+			return {
+				url: animal.url,
+				photos: animal.photos[0].medium
 
-	
-      };
-    });
-  
+
+
+			};
+		});
+
 	catImages.forEach(function (cat) {
 
-		        // Creates a container for cat images, appends it to document, and gives it a class for CSS purposes.
-				var catContainer = document.createElement('div');
-				var catImage = document.createElement('img');
-				
-				catImage.src = cat.photos;
-				catContainer.classList.add('cat-container');
-				document.body.appendChild(catContainer);
-		
-				// Sources and creates images inside the catContainers and sources them.
-				
-				
-		
-				// Appends catImages to catContainers
-				catContainer.appendChild(catImage);
-			
+		// Creates a container for cat images, appends it to document, and gives it a class for CSS purposes.
+		var catContainer = document.createElement('div');
+		var catImage = document.createElement('img');
 
-		catImage.onclick = function () {
-			fetch('https://joke.deno.dev', {
-				method: 'GET',
-			}).then(function (resp) {
-				return resp.json();
+		catImage.src = cat.photos;
+		catContainer.classList.add('cat-container');
 
-			}).then(function (joke) {
 
-				console.log(joke);
+		document.querySelector("#main-container").append(catContainer);
 
-		
+		// Sources and creates images inside the catContainers and sources them.
 
-				var jokeContainer = document.createElement('div');
-				jokeContainer.classList.add('joke-container');
-		
-				var jokeText = document.createElement('p');
-				jokeText.innerText = joke.setup + " " + joke.punchline;
-		
-				var button = document.createElement('button');
-				button.innerText = 'Adopt Me!';
-				button.onclick = function() { 
-					window.open(cat.url);
-				};
-		
-				jokeContainer.appendChild(jokeText);
-				jokeContainer.appendChild(button);
-		
-				catContainer.appendChild(jokeContainer);
-			});
-		  };
-		});
+
+
+		// Appends catImages to catContainers
+		catContainer.appendChild(catImage);
+
+
+		catImage.onclick = function (event) {
+
+			var parentContainer = event.target.parentElement;
+			console.log(parentContainer)
+
+			var numberOfChildren = parentContainer.children.length;
+
+			console.log(numberOfChildren)
+
+			if (numberOfChildren == 1) {
+				fetch('https://joke.deno.dev', {
+					method: 'GET',
+				}).then(function (resp) {
+					return resp.json();
+
+				}).then(function (joke) {
+
+					console.log(joke);
+
+
+
+					var jokeContainer = document.createElement('div');
+					jokeContainer.classList.add('joke-container');
+
+					var jokeText = document.createElement('p');
+					jokeText.innerText = joke.setup + " " + joke.punchline;
+
+					var button = document.createElement('button');
+					button.innerText = 'Adopt Me!';
+					button.onclick = function () {
+						window.open(cat.url);
+					};
+
+					jokeContainer.appendChild(jokeText);
+					jokeContainer.appendChild(button);
+					catContainer.appendChild(jokeContainer);
+				});
+			}
+
+		};
 	});
+});
